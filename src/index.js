26fe20a2050ca0dfc4a4ad4b16ab9df2a30f9d8f
Angel Romero <mrangelrcd@gmail.com>
2022-10-06 01:42:00 +0200

    Apartado visual

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import _ from "lodash-es";
-import "lodash-es/upperCase.js";
-import "lodash-es/shuffle.js";
-import "lodash-es/range.js";
+const newNumberButton = document.querySelector(".new-number-button");
+const playerBoxes = document.querySelectorAll(".player .box");
+const cpuBoxes = document.querySelectorAll(".cpu .box");
+
+function createRandom90Array() {
+  const result = _.range(1, 91);
+  result = _.shuffle(result);
+  return result;
+}
+
+console.log(createRandom90Array);
