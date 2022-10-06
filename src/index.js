7c603c69182c120b597ecc4731c07c6308d24315
Angel Romero <mrangelrcd@gmail.com>
2022-10-06 07:23:28 +0200

    Aparecen numeros en los cartones

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+import _ from "lodash-es";
+import "lodash-es/upperCase.js";
+import "lodash-es/shuffle.js";
+import "lodash-es/range.js";
+import * as modules from "./modules.js";
+
+// selectores DOM
 const newNumberButton = document.querySelector(".new-number-button");
 const playerBoxes = document.querySelectorAll(".player .box");
 const cpuBoxes = document.querySelectorAll(".cpu .box");
 
-function createRandom90Array() {
-  const result = _.range(1, 91);
-  result = _.shuffle(result);
-  return result;
+//
+const playerCard = modules.createRandom15Array();
+const cpuCard = modules.createRandom15Array();
+
+function fillCard(selector, numbersToFill) {
+  for (let i = 0; i < numbersToFill.length; i++) {
+    selector[i].textContent = numbersToFill[i];
+  }
 }
 
-console.log(createRandom90Array);
+fillCard(playerBoxes, playerCard);
+fillCard(cpuBoxes, cpuCard);
---
+++ src/modules.js
@@ -0,0 +1,18 @@
+import _ from "lodash-es";
+import "lodash-es/upperCase.js";
+import "lodash-es/shuffle.js";
+import "lodash-es/range.js";
+
+export function createRandom90Array() {
+  let result = _.range(1, 91);
+  result = _.shuffle(result);
+  return result;
+}
+
+export function createRandom15Array() {
+  let result = createRandom90Array();
+  result = result.slice(0, 15);
+  return result;
+}
+
+console.log(createRandom15Array());
